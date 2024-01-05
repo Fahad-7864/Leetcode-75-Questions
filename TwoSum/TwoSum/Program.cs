1c5dc052f7fb7e45f4d3ce5d3b3f45500e292f79
Fahad-7864 <Fahad-7864@hotmail.com>
2024-01-05 20:47:36 +0000

    Completed two of the Leetcode Grind Questions
    https://www.techinterviewhandbook.org/grind75

---
+++ TwoSum/TwoSum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Solution
+{
+    public static void Main()
+    {
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+
+        int[] result = TwoSum(nums, target);
+
+        // Print the result
+        Console.WriteLine($"Indices: {result[0]}, {result[1]}");
+    }
+
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        throw new ArgumentException("No two sum solution");
+    }
+}
