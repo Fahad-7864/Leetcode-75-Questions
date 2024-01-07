731476a2817b213fdae3db504275eedb9b4cd782
Fahad-7864 <Fahad-7864@hotmail.com>
2024-01-07 16:05:21 +0000

    Added Another potential IQ in the form of how to check if a string is a palindrome

---
+++ IsPalindrome/IsPalindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿// Function to check if a given string 'word' is a palindrome.
+bool IsPalindrome(string word)
+{
+    // Initialize start,end to their character index.
+    int start = 0;
+    int end = word.Length - 1;
+
+    while (start < end)
+    {
+        // Check if characters at 'start' and 'end' are different.
+        if (word[start] != word[end])
+        {
+            // If they are different, it's not a palindrome, return false.
+            return false;
+        }
+        start++; // Move 'start' to the next character.
+        end--;   // Move 'end' to the previous character.
+    }
+
+    // If the loop completes without finding mismatched characters, return true.
+    // It means the word is a palindrome.
+    return true;
+}
+
+string[] words = { "racecar", "talented", "deified", "tent", "tenet" };
+
+Console.WriteLine("Is it a palindrome?");
+foreach (string word in words)
+{
+    Console.WriteLine($"{word}: {IsPalindrome(word)}");
+}
